42637ef3ac2759233e87984b07e2232e4d7def5f
Dexter Haslem <dmh@fastmail.com>
2021-03-28 16:33:24 -0600

    Initial Commit

---
+++ osxscratch_c/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  osxscratch_c
+//
+//  Created by Dexter Haslem on 3/28/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
